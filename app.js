8ca58f2e15c9dccadc744161d089108b3616d654
Eli Shalom <eli.shalom@gmail.com>
2020-04-19 15:09:09 +0300

    Arranged code and static resources

--- app.js
+++ app.js
@@ -1,82 +1,44 @@
 "use strict";
-// Optional. You will see this name in eg. 'ps' or 'top' command
 process.title = 'youtube-party';
-
 const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
-// websocket and http servers
-const { Server } = require('ws');
 
-
-
-// var webSocketServer = require('websocket').server;
+var webSocketServer = require('websocket').server;
 var uuid = require('uuid');
 var express = require('express');
 
-/**
- * Global variablesheruko
- */
-// list of currently connected clients (users)
 var clients = [ ];
 
 const server = express()
-    .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+    .use(express.static('client'))
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-// var wsServer = new webSocketServer({
-//     httpServer: server
-// });
-const wss = new Server({ server });
+var wsServer = new webSocketServer({
+    httpServer: server
+});
 
-wss.on('connection', (ws) => {
-    console.log('Client connected');
-    ws.id = uuid.v4();
-    clients.push(ws);
+wsServer.on('request', function(request) {
+    console.log((new Date()) + ' Connection from origin ' + request.origin + '.');
+    var connection = request.accept(null, request.origin);
+    connection.id = uuid.v4();
+    clients.push(connection);
+    console.log((new Date()) + ' Connection accepted.');
 
-    ws.on('message', function (message) {
-        // broadcast message to all connected clients
-        console.log("from" + " " + connection.id);
+    connection.on('message', function(message) {
+        console.log("from" + " " + connection.id );
         for (var i = 0; i < clients.length; i++) {
             if (clients[i].id !== connection.id) {
-                console.log("to" + " " + clients[i].id);
+                console.log("to" + " " + clients[i].id );
                 clients[i].send(message.utf8Data);
             }
         }
     });
 
-
-    ws.on('close', () => {
+    connection.on('close', function() {
         console.log((new Date()) + " Peer "
             + connection.remoteAddress + " disconnected.");
-        // remove user from the list of connected clients
         clients = clients.filter(conn => conn.id !== connection.id);
     });
-})
-// wsServer.on('request', function(request) {
-//     console.log((new Date()) + ' Connection from origin ' + request.origin + '.');
-//     var connection = request.accept(null, request.origin);
-//     connection.id = uuid.v4();
-//     clients.push(connection);
-//     console.log((new Date()) + ' Connection accepted.');
-//
-//     connection.on('message', function(message) {
-//         // broadcast message to all connected clients
-//         console.log("from" + " " + connection.id );
-//         for (var i = 0; i < clients.length; i++) {
-//             if (clients[i].id !== connection.id) {
-//                 console.log("to" + " " + clients[i].id );
-//                 clients[i].send(message.utf8Data);
-//             }
-//         }
-//     });
-//     // user disconnected
-//     connection.on('close', function() {
-//         console.log((new Date()) + " Peer "
-//             + connection.remoteAddress + " disconnected.");
-//         // remove user from the list of connected clients
-//         clients = clients.filter(conn => conn.id !== connection.id);
-//     });
-// });
+});
 
 
 
--- client/client.js
+++ client/client.js
@@ -1,43 +1,28 @@
-// $(function () {
 "use strict";
-// for better performance - to avoid searching in DOM
+
 var videoId = "";
-var lastSentMessage = null;
-// if user is running mozilla then use it's built-in WebSocket
+var recentRecievedMessage = 0;
+
 window.WebSocket = window.WebSocket || window.MozWebSocket;
-// if browser doesn't support WebSocket, just show
-// some notification and exit
 if (!window.WebSocket) {
     content.html($('<p>',
         { text:'Sorry, but your browser doesn\'t support WebSocket.'}
     ));
     input.hide();
     $('span').hide();
-    // return;
 }
-// open connection
-// var connection = new WebSocket('ws://127.0.0.1');
 
-var HOST = location.origin.replace(/^http/, 'ws')
+var HOST = location.origin.replace(/^http/, 'ws');
 var ws = new WebSocket(HOST);
-ws.onopen = function () {
-};
 ws.onerror = function (error) {
-    // just in there were some problems with connection...
     content.html($('<p>', {
         text: 'Sorry, but there\'s some problem with your '
             + 'connection or the server is down.'
     }));
 };
-// most important part - incoming messages
+
 ws.onmessage = function (message) {
-    // try to parse JSON message. Because we know that the server
-    // always returns JSON this should work without any problem but
-    // we should make sure that the massage is not chunked or
-    // otherwise damaged.
-    if (Date.now() - lastSentMessage < 500){
-        return;
-    }
+    recentRecievedMessage = Date.now();
     try {
         var json = JSON.parse(message.data);
         if (json.event === 'play'){
@@ -51,14 +36,46 @@ ws.onmessage = function (message) {
         console.log('Invalid JSON: ', message.data);
     }
 };
-/**
- * Send message when user presses Enter key
- */
+
 function broadcast(msg){
     if (!msg) {
         return;
     }
-    // send the message as an ordinary text
     ws.send(msg);
 }
-// });
+
+var player;
+var urlParams = new URLSearchParams(window.location.search);
+videoId = urlParams.get('v');
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        videoId: videoId,
+        events: {
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+function onPlayerStateChange(event) {
+    if (Date.now() - recentRecievedMessage < 1000){
+        return;
+    }
+    if (event.data === YT.PlayerState.PLAYING) {
+        var obj = {
+            time: player.getCurrentTime(),
+            event: "play"
+        };
+        broadcast(JSON.stringify(obj));
+    }
+    else if (event.data === YT.PlayerState.PAUSED){
+        var obj = {
+            event: "pause"
+        };
+        broadcast(JSON.stringify(obj));
+    }
+}
+
+function stopVideo() {
+    player.stopVideo();
+}
