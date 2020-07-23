8e198f87169db2a37cf6df65050d89fc74100fa0
Lena Kaplan <lena.kaplan@similarweb.com>
2020-07-23 20:17:05 +0300

    add ping pong for keeping websockets open even events are not beeing sent +use ws

--- app.js
+++ app.js
@@ -2,52 +2,46 @@
 process.title = 'youtube-party';
 const PORT = process.env.PORT || 3000;
 
-var webSocketServer = require('websocket').server;
-var uuid = require('uuid');
+var WebSocket = require('ws');
 var express = require('express');
 
-var clients = [ ];
 
 const server = express()
     .use(express.static('client'))
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-var wsServer = new webSocketServer({
-    httpServer: server
-});
+function noop() {}
 
-wsServer.on('request', function(request) {
-    console.log((new Date()) + ' Connection from origin ' + request.origin + '.');
-    var connection = request.accept(null, request.origin);
-    connection.id = uuid.v4();
-    clients.push(connection);
-    console.log((new Date()) + ' Connection accepted.');
+function heartbeat() {
+    this.isAlive = true;
+}
+const wss = new WebSocket.Server({server:server});
 
-    connection.on('message', function(message) {
-        console.log("from" + " " + connection.id );
-        for (var i = 0; i < clients.length; i++) {
-            if (clients[i].id !== connection.id) {
-                console.log("to" + " " + clients[i].id );
-                clients[i].send(message.utf8Data);
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(data) {
+        wss.clients.forEach(function each(client) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(data);
             }
-        }
+        });
     });
 
-    connection.on('close', function() {
-        console.log((new Date()) + " Peer "
-            + connection.remoteAddress + " disconnected.");
-        clients = clients.filter(conn => conn.id !== connection.id);
-    });
+    ws.isAlive = true;
+    ws.on('pong', heartbeat);
 });
 
+const interval = setInterval(function ping() {
+    wss.clients.forEach(function each(ws) {
+        if (ws.isAlive === false) return ws.terminate();
 
+        ws.isAlive = false;
+        ws.ping(noop);
+    });
+}, 30000);
 
-
-
-
-
-
-
+wss.on('close', function close() {
+    clearInterval(interval);
+});
 
 
 
